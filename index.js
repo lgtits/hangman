b9bab069bdd6af4d17af3756ac2fed1cadd2a9a5
lgtits <a0937370704@gmail.com>
2022-07-04 11:22:06 +0800

    add definition

--- index.js
+++ index.js
@@ -1,118 +1,155 @@
-const getWord = document.querySelector('.getWord')
-// const word = document.querySelector('.word')
-const board = document.querySelector('.board')
-// const guess = document.querySelector('.guess')
-const letters = document.querySelectorAll('.letter')
-const letter1 = document.querySelector('.letter1')
-const letter2 = document.querySelector('.letter2')
-const letter3 = document.querySelector('.letter3')
-const letter4 = document.querySelector('.letter4')
-const letter5 = document.querySelector('.letter5')
-const inputs = document.querySelectorAll('input')
-// const choices = document.querySelector('.choices')
-const labels = document.querySelectorAll('label')
+const getWord = document.querySelector(".getWord");
+const board = document.querySelector(".board");
+const letters = document.querySelectorAll(".letter");
+const letter1 = document.querySelector(".letter1");
+const letter2 = document.querySelector(".letter2");
+const letter3 = document.querySelector(".letter3");
+const letter4 = document.querySelector(".letter4");
+const letter5 = document.querySelector(".letter5");
+const inputs = document.querySelectorAll("input");
+const labels = document.querySelectorAll("label");
+const wrong = document.querySelector(".wrong");
+const win = document.querySelector(".win");
+const description = document.querySelector(".description");
+const vocabularies = document.querySelector(".vocabularies");
+let definitionDescription;
+let answer;
+let split;
+let wrongTime = 0;
+let correct = 0;
+let winTime = 0;
+let processing = false;
 
-let answer
-let split
-let wrongTime = 0
-let correct = 0
-let winTime = 0
-const wrong = document.querySelector('.wrong')
-const win = document.querySelector('.win')
-
-let url = 'https://random-word-api.herokuapp.com/word?length=5'
+let url = "https://random-word-api.herokuapp.com/word?length=5";
 
 async function getData(url) {
-  console.log('game starts')
+  console.log("game starts");
+  processing = true;
+
+  if (answer) {
+    console.log(answer);
+    vocabularies.innerHTML += `<li>
+      <span class="vocabulary">${answer}: </span>
+      <span class="meaning">${definitionDescription}</span>
+    </li>`;
+  }
   try {
-    let response = await fetch(url)
+    let response = await fetch(url);
     let data = await response.json();
-    answer = data[0]
-    console.log('answer: ', answer)
-    split = [...data[0]]
-    // console.log(split)
-    letter1.innerText = ''
-    letter2.innerText = ''
-    letter3.innerText = ''
-    letter4.innerText = ''
-    letter5.innerText = ''
-    inputs.forEach(item => {
-      item.removeAttribute("disabled")
-    })
-    // word.innerText = answer.toUpperCase()
-    wrongTime = 0
-    wrong.innerText = wrongTime
+    answer = data[0];
+    console.log("answer: ", answer);
+    split = [...data[0]];
+    letter1.innerText = "";
+    letter2.innerText = "";
+    letter3.innerText = "";
+    letter4.innerText = "";
+    letter5.innerText = "";
+    inputs.forEach((item) => {
+      item.removeAttribute("disabled");
+    });
+    labels.forEach((item) => {
+      item.removeAttribute("disabled");
+    });
+    wrongTime = 0;
+    wrong.innerText = wrongTime;
+    getDefinition(answer);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-getWord.addEventListener('click', function () {
-  getData(url)
-})
+async function getDefinition(word) {
+  console.log("get definition of:", word);
+  try {
+    let response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    let definition = await response.json();
+    console.log(
+      "definition:",
+      definition[0].meanings[0].definitions[0].definition
+    );
+    definitionDescription = definition[0].meanings[0].definitions[0].definition;
+    description.innerText = definitionDescription;
+    processing = false;
+  } catch (error) {
+    console.log("Error:", error);
+    answer = ''
+    getData(url);
+    // definitionDescription = "no definition.";
+    // description.innerText = definitionDescription;
+    // processing = false;
+  }
+}
 
-// guess.addEventListener('click', function () {
-//   let guessLetter = document.querySelectorAll('input[name="choice"]')
-//   console.log(guessLetter)
-//   guessLetter.forEach(letter => {
-//     if (letter.checked) {
-//       console.log('select:', letter.value)
-//       check(letter.value)
-//     }
-//   })
-// })
+getWord.addEventListener("click", function () {
+  if (processing === true) {
+    return;
+  }
+  getData(url);
+});
 
 function check(letter) {
   if (split[0] === letter) {
-    letter1.innerText = letter
-    correct++
+    letter1.innerText = letter;
+    correct++;
   }
   if (split[1] === letter) {
-    letter2.innerText = letter
-    correct++
+    letter2.innerText = letter;
+    correct++;
   }
   if (split[2] === letter) {
-    letter3.innerText = letter
-    correct++
+    letter3.innerText = letter;
+    correct++;
   }
   if (split[3] === letter) {
-    letter4.innerText = letter
-    correct++
+    letter4.innerText = letter;
+    correct++;
   }
   if (split[4] === letter) {
-    letter5.innerText = letter
-    correct++
+    letter5.innerText = letter;
+    correct++;
   }
-  if (split[0] === letter || split[1] === letter || split[2] === letter || split[3] === letter || split[4] === letter){
-    inputs[letter.charCodeAt(0) - 97].setAttribute("disabled", true)
+  if (
+    split[0] === letter ||
+    split[1] === letter ||
+    split[2] === letter ||
+    split[3] === letter ||
+    split[4] === letter
+  ) {
+    inputs[letter.charCodeAt(0) - 97].setAttribute("disabled", true);
+    labels[letter.charCodeAt(0) - 97].setAttribute("disabled", true);
   }
   if (correct === 5) {
-    alert(`Bingo！ Answer is ${answer}.`)
-    correct = 0
-    getData(url)
-    winTime++
-    win.innerText = winTime
+    alert(`Bingo！ Answer is ${answer}.`);
+    correct = 0;
+    getData(url);
+    winTime++;
+    win.innerText = winTime;
   }
-  if (split[0] !== letter && split[1] !== letter && split[2] !== letter && split[3] !== letter && split[4] !== letter) {
-    // console.log(inputs)
-    inputs[letter.charCodeAt(0) - 97].setAttribute("disabled", true)
-    wrongTime++
-    wrong.innerText = wrongTime
+  if (
+    split[0] !== letter &&
+    split[1] !== letter &&
+    split[2] !== letter &&
+    split[3] !== letter &&
+    split[4] !== letter
+  ) {
+    inputs[letter.charCodeAt(0) - 97].setAttribute("disabled", true);
+    labels[letter.charCodeAt(0) - 97].setAttribute("disabled", true);
+    wrongTime++;
+    wrong.innerText = wrongTime;
   }
-
 }
 
-// choices.addEventListener('click', function(e){
-//   console.log(e.target.value)
-// })
-
-labels.forEach(label => {
-  label.addEventListener('click', function (e) {
-    if(!answer){
-      return
+labels.forEach((label) => {
+  label.addEventListener("click", function (e) {
+    if (!answer) {
+      return;
     }
-    let letter = e.target.getAttribute('for')
-    // console.log('test', letter)
-    check(letter)
-  })
-})
+    if (e.target.getAttribute("disabled")) {
+      return;
+    }
+    let letter = e.target.getAttribute("for");
+    check(letter);
+  });
+});
